3e6c9142af04dbaba47d1b44323aa4cf537c4463
williamsimms <williamsimms193@gmail.com>
2021-04-29 11:04:45 -0500

    added methods

--- vector.hpp
+++ vector.hpp
@@ -2,6 +2,7 @@
 #include <initializer_list>
 #include <iostream>
 #include <iterator>
+#include <ostream>
 #include <utility>
 #include <vector>
 
@@ -10,20 +11,72 @@ using std::endl;
 using std::forward;
 using std::initializer_list;
 using std::move;
+using std::ostream;
 
 template <typename T>
 class Vector {
   using ValueType = T;
   using PointerType = T*;
+  using ConstPointer = const T*;
   using ReferenceType = T&;
+  using ConstReferenceType = const T&;
+
+ private:
+  int size;
+  int capacity;
+  T* data;
 
  public:
   Vector();
+  Vector(T);
+  Vector(vector<T>);
   Vector(initializer_list<T>);
   Vector(const Vector&);
   Vector(Vector&&);
   ~Vector();
 
+  void Assign();
+
   void PushBack(const T&);
   void PushBack(T&&);
+
+  void PushFront(const T&);
+  void PushFront(T&&);
+
+  void PushAt(int index, const T&);
+  void PushAt(int index, T&&);
+
+  void PopFront();
+  void PopBack();
+
+  void EmplaceBack();
+  void EmplaceFront();
+  void EmplaceAt();
+
+  int Size();
+  int MaxSize();
+  int Capacity();
+
+  bool Empty();
+
+  void Reserve();
+  void ShrinkToFit();
+  void Clear();
+
+  T Front();
+  T Back();
+  T Middle();
+
+  void Sort();
+  void Reverse();
+
+  void At();
+
+  void Swap();
+
+  void RemoveIf();
+
+  void Print();
+
+  ostream& operator<<(ostream& os, const Vector<T>& vector);
 };
